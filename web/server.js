5a4cfb61560799a654e7a901bb8575135683650f
lennytubby <lennart@slusny.de>
2020-03-22 22:19:35 +0100

    neu strukturiert

--- web/server.js
+++ web/server.js
@@ -8,9 +8,13 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+app.use(express.static('html'))
+
+/*
 app.get('/', (req, res) => {
   res.send('Hello World2');
 });
+*/
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
